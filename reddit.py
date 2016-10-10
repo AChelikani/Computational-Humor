ecc2564e1939be2e108016b610e0c3c816895960
Rahul Bachal <rbachal@caltech.edu>
2016-10-10 16:53:00 -0700

    Added command line arguments for subreddit and count, getting comments

--- reddit.py
+++ reddit.py
@@ -1,3 +1,4 @@
+import argparse
 import praw
 
 # Class for data object
@@ -5,9 +6,10 @@ import praw
 Image url, title
 '''
 class DataObject(object):
-    def __init__(self, title, url):
+    def __init__(self, title, url, comments):
         self.imgurl = url
         self.title = title
+        self.comments = comments
 
 # Wrapper class for Reddit API
 class Reddit(object):
@@ -31,12 +33,20 @@ class Reddit(object):
             data = []
             results = self.reddit.get_subreddit(subName).get_hot(limit=lim)
             for item in results:
-                data.append(DataObject(item.title, item.url))
+                data.append(DataObject(item.title, item.url, item.comments))
             return data
         return None
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("subreddit")
+    parser.add_argument("count", type=int)
+    args = parser.parse_args()
     reddit = Reddit("Computation Humor 1.0")
     reddit.connect()
-    print reddit.getSubreddit("food", 1)
+    x = reddit.getSubreddit(args.subreddit, args.count)
+    for post in x:
+        print post.title
+        print "================================================================================"
